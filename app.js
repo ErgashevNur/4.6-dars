108710ca3fb19d7144d5ab121190a9cc4c473b74
MuhammadNurulloh <dodgechalanger9@gmail.com>
2024-10-04 22:20:26 +0300

    4.6 dars

---
+++ app.js
@@ -0,0 +1,43 @@
+// let n = 7;
+// let a = 3;
+// let b = 5;
+
+// let result = [];
+// result[0] = a;
+// result[1] = b;
+
+// // console.log(result)
+
+// for (let i = 0; i < n; i++) {
+//   let counter = 0;
+//   for (let k = 0; k < result.length; k++) {
+//     counter += result[k];
+//   }
+
+//   result.push(counter);
+// }
+
+// Array10
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// let n = arr.length;
+
+// for (let i = 0; i < Math.ceil(n / 20); i++) {
+//   if (i < n) {
+//     console.log(arr[i]);
+//   }
+//   if (n - 1 - i > i) {
+//     console.log(arr[n - 1 - i]);
+//   }
+// }
+
+// Uyga vazifa
+// 1
+// function numbers(a, b) {
+//   return a + b;
+// }
+// let num = numbers(10, 20);
+// console.log(num);
+
+// 2
+//
