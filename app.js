dd4b78d1bdc5f99694b1a3b2aa51d3f047f2a271
MuhammadNurulloh <dodgechalanger9@gmail.com>
2024-10-05 21:43:16 +0300

    4.6 dars

--- app.js
+++ app.js
@@ -32,6 +32,7 @@
 // }
 
 // Uyga vazifa
+
 // 1
 // function numbers(a, b) {
 //   return a + b;
@@ -40,4 +41,142 @@
 // console.log(num);
 
 // 2
-//
+// function numbers(minut) {
+//   return minut * 60;
+// }
+// let num = numbers(20);
+// console.log(num);
+
+// 3
+// function numbers(a) {
+//   return a + 1;
+// }
+// let num = numbers(10);
+// console.log(num);
+
+// 4
+// function numbers(a, b) {
+//   return (a * b) / 2;
+// }
+// let num = numbers(10, 20);
+// console.log(num);
+
+// 5
+// function numbers(a) {
+//   return a * 365;
+// }
+// let num = numbers(10);
+// console.log(num);
+
+// 6
+// function numbers(a) {
+//   return a * a * a;
+// }
+// let num = numbers(10);
+// console.log(num);
+
+// 7
+// function numbers(a) {
+//   return a[0];
+// }
+// let num = [10, 20, 30, 40];
+// console.log(numbers(num));
+
+// 8
+// function numbers(a, b) {
+//   return a * b;
+// }
+// let num = numbers(10, 20);
+// console.log(num);
+
+// 9
+// function numbers(a) {
+//   return a * 3600;
+// }
+// let num = numbers(10);
+// console.log(num);
+
+// 10
+// function numbers(a, b) {
+//   return a * b - 1;
+// }
+// let num = numbers(10, 20);
+// console.log(num);
+
+// 11
+// function numbers(a, b) {
+//   return a % b;
+// }
+// console.log(numbers(90, 20));
+
+// 12
+// function S(a, b) {
+//   return a * b;
+// }
+// console.log(S(10, 20));
+
+// 13
+// function number() {
+//   let a = prompt("Ismingni kirit");
+//   let b = "Something ";
+//   return b + a;
+// }
+// console.log(number());
+
+// 14
+// function num() {
+//   let a = prompt("Son kiriting");
+//   return a * a;
+// }
+// console.log(num());
+
+// 15
+// function num() {
+//   let a = prompt("Son kiriting");
+//   if (a > 0) {
+//     console.log("Folse");
+//   } else if (a <= 0) {
+//     console.log("True");
+//   }
+//   return a;
+// }
+// console.log(num());
+
+// 16
+// function num() {
+//   let a = prompt("Burchaklar sonini kiriting");
+//   return (a - 2) * 180;
+// }
+// console.log(num());
+
+// 17
+// function num() {
+//   let a = prompt("Achko sonini kiriting");
+//   let b = prompt("Achko sonini kiriting");
+//   return a * 2 + b * 3;
+// }
+// console.log(num());
+
+// 18
+// function number() {
+//   let a = prompt("Ismingni kirit");
+//   let b = "Edabit";
+//   return a + b;
+// }
+// console.log(number());
+
+// 19
+// function number() {
+//   let a = parseInt(prompt("Ismingni kirit"));
+//   let b = parseInt(prompt("Ismingni kirit"));
+//   let son = 0;
+//   son = a + b;
+
+//   if (son <= 100) {
+//     console.log("True");
+//   } else if (son > 100) {
+//     console.log("False");
+//   }
+//   return son;
+// }
+// console.log(number());
